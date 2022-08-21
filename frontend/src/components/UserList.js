c673e4162701ea93ea355e911627d720049f7397
ukkuji <andi.syukur1990@hotmail.com>
2022-08-21 18:42:35 +0800

    tes

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -35,7 +35,7 @@ const UserList = () => {
   return (
     <div className="columns mt-5 is-centered">
       <div className="column is-half">
-        <Banner title="Users List" />
+        <Banner title="Users List daftar" />
         <Link to={`add`} className="button is-info">
           Add New
         </Link>
